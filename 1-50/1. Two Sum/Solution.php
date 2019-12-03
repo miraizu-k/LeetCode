acf4cc84f6beb16c21cfad68f94bc4b0e397e5fd
Kenichi Yoshida <yoshida@anysys.jp>
2019-12-03 20:48:58 +0900

    PHPの解答追加

--- 1-50/1. Two Sum/Solution.php
+++ 1-50/1. Two Sum/Solution.php
@@ -8,6 +8,23 @@ class Solution {
      * @return Integer[]
      */
     function twoSum($nums, $target) {
-        return [0,1];
+        $indexes = array_flip($nums);
+        foreach ($nums as $num) {
+            $val = $target - $num;
+            $index = isset($indexes[$val]) && is_numeric($indexes[$val]) ? $indexes[$val] : null;
+            if (is_null($index)) {
+                continue;
+            }
+            if ($val !== $num) {
+                return [$indexes[$num], $index];
+            }
+
+            unset($nums[$index]);
+            $newIndexes = array_flip($nums);
+            if (isset($newIndexes[$num])) {
+                return [$newIndexes[$num], $index];
+            }
+        }
+        return [];
     }
 }
--- 1-50/1. Two Sum/SolutionTest.php
+++ 1-50/1. Two Sum/SolutionTest.php
@@ -11,4 +11,22 @@ class SolutionTest extends TestCase
         $solution = new Solution();
         $this->assertEquals($solution->twoSum([2, 7, 11, 15], 9), [0, 1]);
     }
+
+    public function testTwoSumTwo()
+    {
+        $solution = new Solution();
+        $this->assertEquals($solution->twoSum([3, 3], 6), [0, 1]);
+    }
+
+    public function testTwoSumThree()
+    {
+        $solution = new Solution();
+        $this->assertEquals($solution->twoSum([3, 1, 3], 6), [0, 2]);
+    }
+
+    public function testTwoSumFor()
+    {
+        $solution = new Solution();
+        $this->assertEquals($solution->twoSum([3, 2, 4], 6), [1, 2]);
+    }
 }
